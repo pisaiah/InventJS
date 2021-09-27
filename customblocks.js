800545608312051605ce4a5c1aba8147154e9822
Isaiah <isaiahpatton1@gmail.com>
2021-09-26 20:50:21 -0400

    Position blocks

--- customblocks.js
+++ customblocks.js
@@ -146,3 +146,67 @@ Blockly.JavaScript['js_onclick'] = function(block) {
   return 'document.getElementById(' + argument0 + ').onclick = function() {' + argument1.replaceAll("'", "") + '}' + ";";
 };
 
+
+Blockly.Blocks['js_top'] = {
+  init: function() {
+    this.appendValueInput('VAL1')
+      .appendField('getElementById(');
+    this.appendValueInput('VAL2')
+      .appendField(').style.top =');
+    this.setInputsInline(true);
+    this.setPreviousStatement(!0);
+    this.setNextStatement(!0);
+    this.setOutput(0, 'Dummy');
+    this.setColour(160);
+    this.setTooltip('Set the content of an element');
+  }
+};
+Blockly.JavaScript['js_top'] = function(block) {
+  var argument0 = Blockly.JavaScript.valueToCode(block, 'VAL1', Blockly.JavaScript.ORDER_FUNCTION_CALL) || '\'\'';
+  var argument1 = Blockly.JavaScript.valueToCode(block, 'VAL2', Blockly.JavaScript.ORDER_FUNCTION_CALL) || '\'\'';
+  return 'document.getElementById(' + argument0 + ').style.top = ' + argument1 + ";";
+};
+
+Blockly.Blocks['js_left'] = {
+  init: function() {
+    this.appendValueInput('VAL1')
+      .appendField('getElementById(');
+    this.appendValueInput('VAL2')
+      .appendField(').style.left =');
+    this.setInputsInline(true);
+    this.setPreviousStatement(!0);
+    this.setNextStatement(!0);
+    this.setOutput(0, 'Dummy');
+    this.setColour(160);
+    this.setTooltip('Set the content of an element');
+  }
+};
+Blockly.JavaScript['js_left'] = function(block) {
+  var argument0 = Blockly.JavaScript.valueToCode(block, 'VAL1', Blockly.JavaScript.ORDER_FUNCTION_CALL) || '\'\'';
+  var argument1 = Blockly.JavaScript.valueToCode(block, 'VAL2', Blockly.JavaScript.ORDER_FUNCTION_CALL) || '\'\'';
+  return 'document.getElementById(' + argument0 + ').style.left = ' + argument1 + ";";
+};
+
+Blockly.Blocks['js_pos'] = {
+  init: function() {
+    this.appendValueInput('VAL1')
+      .appendField('getElementById(');
+    this.appendValueInput('VAL2')
+      .appendField(').style.position =');
+    this.setInputsInline(true);
+    this.setPreviousStatement(!0);
+    this.setNextStatement(!0);
+    this.setOutput(0, 'Dummy');
+    this.setColour(160);
+    this.setTooltip('Set the content of an element');
+  }
+};
+Blockly.JavaScript['js_pos'] = function(block) {
+  var argument0 = Blockly.JavaScript.valueToCode(block, 'VAL1', Blockly.JavaScript.ORDER_FUNCTION_CALL) || '\'\'';
+  var argument1 = Blockly.JavaScript.valueToCode(block, 'VAL2', Blockly.JavaScript.ORDER_FUNCTION_CALL) || '\'\'';
+  return 'document.getElementById(' + argument0 + ').style.position = ' + argument1 + ";";
+};
+
+
+
+
